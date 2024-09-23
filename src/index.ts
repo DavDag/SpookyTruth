1e0c577dc7400d15a7508d99e187ebf5d2bf4254
Davide Risaliti <davdag24@gmail.com>
2024-09-23 12:30:48 +0200

    improvements

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,3 @@ window.addEventListener('load', () => {
     MyApp.Initialize()
     MyApp.Start()
 })
-
-const obs = new MutationObserver(() => {
-    const btn = document.getElementById('excalibur-play')
-    document.getElementById('custom-start-overlay-play-btn').style.display =
-        !!btn ? 'block' : 'none'
-})
-
-obs.observe(document.body, { childList: true })
-document.getElementById('custom-start-overlay-play-btn').onclick = () => {
-    document.getElementById('excalibur-play').click()
-    document.getElementById('custom-start-overlay').style.display = 'none'
-}
